d6ff86f82ec42447305a8aacba4230075614b7d8
Eric Njiraini <menjiraini@gmail.com>
2025-07-15 15:22:32 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/me">
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* Add any additional routes here */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
